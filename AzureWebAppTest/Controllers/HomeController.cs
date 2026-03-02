8b70beaa272f10088d79ede7c0673953f72f02e3
narkul1176 <narkul1176@gmail.com>
2026-03-02 15:10:36 +0530

    Add project files.

---
+++ AzureWebAppTest/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+using System.Diagnostics;
+using AzureWebAppTest.Models;
+using Entities.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureWebAppTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var products = _context.Products.ToList();
+
+            return View(products);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
